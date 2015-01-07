e1e150e40b0783666d124fc9c843e7c95c950e17
Viktor <vic99999@yandex.ru>
2015-01-07 17:59:19 +0600

    Update BigInteger.js

--- BigInteger.js
+++ BigInteger.js
@@ -65,11 +65,10 @@
   var fh = 134217728;
   var sh = 67108864;
   if (Number.MAX_SAFE_INTEGER !== undefined && Number.EPSILON !== undefined) {
-  debugger;
     var maxValue = min(Number.MAX_SAFE_INTEGER + 1, floor(2 / Number.EPSILON));
     var n = floor(floor(log(maxValue) / log(2) - 0.5) / 2);
-    var sh = pow(2, n);
-    var fh = sh;
+    fh = pow(2, n);
+    sh = fh;
     if (fh * 2 <= floor(maxValue / fh)) {
       fh *= 2;
     }

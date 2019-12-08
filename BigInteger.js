6daaef9d2f2295f2249c6d5d82e211bb4ec3a991
Yaffle <vic99999@yandex.ru>
2019-12-08 17:19:04 +0100

    update

--- BigInteger.js
+++ BigInteger.js
@@ -151,7 +151,7 @@
       sign = 1;
     }
     var radix = 10;
-    if (from === 0 && s.length >= 2 && s.charCodeAt(0) === 48) {
+    if (from === 0 && length >= 2 && s.charCodeAt(0) === 48) {
       if (s.charCodeAt(1) === 98) {
         radix = 2;
         from = 2;
@@ -659,30 +659,36 @@
     return a > b;
   };
 
-  BigIntWrapper.exponentiate = function (a, b) {
-    var n = BigIntWrapper.toNumber(b);
+  BigIntWrapper.exponentiate = function (a, b) { // a**b
+    if (typeof a !== "bigint") {
+      throw new TypeError();
+    }
+    if (typeof b !== "bigint") {
+      throw new TypeError();
+    }
+    var n = Number(b);
     if (n < 0) {
       throw new RangeError();
     }
     if (n > 9007199254740991) {
-      var y = BigIntWrapper.toNumber(a);
+      var y = Number(a);
       if (y === 0 || y === -1 || y === +1) {
-        return BigIntWrapper.BigInt(y === -1 && BigIntWrapper.toNumber(BigIntWrapper.remainder(b, BigIntWrapper.BigInt(2))) === 0 ? +1 : y);
+        return BigInt(y === -1 && Number(b % BigInt(2)) === 0 ? +1 : y);
       }
       throw new RangeError();
     }
-    var accumulator = BigIntWrapper.BigInt(1);
+    var accumulator = BigInt(1);
     if (n > 0) {
       var x = a;
       while (n >= 2) {
         var t = Math.floor(n / 2);
         if (t * 2 !== n) {
-          accumulator = BigIntWrapper.multiply(accumulator, x);
+          accumulator *= x;
         }
         n = t;
-        x = BigIntWrapper.multiply(x, x);
+        x *= x;
       }
-      accumulator = BigIntWrapper.multiply(accumulator, x);
+      accumulator *= x;
     }
     return accumulator;
   };
@@ -787,11 +793,14 @@
     if (typeof x === "number") {
       return x;
     }
-    var value = 0 + Number(x);
-    if (value >= -9007199254740991 && value <= +9007199254740991) {
-      return value;
+    if (typeof x === "string") {
+      var value = 0 + Number(x);
+      if (value >= -9007199254740991 && value <= +9007199254740991) {
+        return value;
+      }
+      return Internal.BigInt(x);
     }
-    return Internal.BigInt(x);
+    throw new RangeError();
   };
   // Conversion to Number:
   BigInteger.toNumber = function (x) {
